49f39ecccee0bbda6f68759d1689862aabc760f3
Max <msuraev@sysmocom.de>
2016-01-19 18:17:35 +0100

    Remove osmo_a5_1 and osmo_a5_2 from public API
    Developers should use generic osmo_a5() instead.

--- include/osmocom/gsm/a5.h
+++ include/osmocom/gsm/a5.h
@@ -54,7 +54,5 @@ osmo_a5_fn_count(uint32_t fn)
 	 *    (converted internally to fn_count)
 	 */
 int osmo_a5(int n, const uint8_t *key, uint32_t fn, ubit_t *dl, ubit_t *ul);
-void osmo_a5_1(const uint8_t *key, uint32_t fn, ubit_t *dl, ubit_t *ul);
-void osmo_a5_2(const uint8_t *key, uint32_t fn, ubit_t *dl, ubit_t *ul);
 
 /*! @} */
